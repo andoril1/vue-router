f592b88b05a0b1ad5037314c8cde53a001e52c46
andoril1 <141507396+andoril1@users.noreply.github.com>
2024-06-17 17:40:27 +0200

    Update eChart

--- src/components/eChart.vue
+++ src/components/eChart.vue
@@ -13,7 +13,8 @@
     GridComponent
   } from 'echarts/components';
   import VChart, { THEME_KEY } from 'vue-echarts';
-  import { ref, provide } from 'vue';
+  import { ref, provide, onMounted } from 'vue';
+  import axios from 'axios';
   
   use([
     CanvasRenderer,
@@ -60,6 +61,68 @@
       },
     ],
   });
+      const chartData = ref([])
+      console.log(chartData)
+      
+    function applyStatsToChart(stats) {
+        var tempChartData = {
+        labels: [],
+        datasets: [
+            {
+                label: 'Pool Hashrate',
+                backgroundColor: '#8A6158',
+                data: []
+            },
+            {
+                label: 'Miners',
+                backgroundColor: '#66E03C',
+                data: []
+            },
+            {
+                label: 'Network Hashrate',
+                backgroundColor: '#f87979',
+                data: []
+            },
+            {
+                label: 'Network Difficulty',
+                backgroundColor: '#3C5AE0',
+                data: []
+            },
+            {
+                label: 'Valid Shares',
+                backgroundColor: '#3C5AE0',
+                data: []
+            },
+        ]
+    }
+
+    stats.forEach((singleStatObj, index)=>{
+        tempChartData.labels[index] = singleStatObj.created  // Format the date here
+        tempChartData.datasets[0].data[index] = singleStatObj.poolHashrate
+        tempChartData.datasets[1].data[index] = singleStatObj.connectedMiners
+        tempChartData.datasets[2].data[index] = singleStatObj.networkHashrate
+        tempChartData.datasets[3].data[index] = singleStatObj.networkDifficulty
+        tempChartData.datasets[4].data[index] = singleStatObj.validSharesPerSecond
+     })
+
+    chartData.value = tempChartData
+    }
+      
+      function getStats() {
+            axios
+            .get('https://pool.flazzard.com/api/pools/pug1/performance')
+            .then((response) => {
+            applyStatsToChart(response.data.stats)
+            //console.log(response.data.stats)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            
+        }
+      onMounted(() => {
+        getStats()
+})
   </script>
   
   <style scoped>
